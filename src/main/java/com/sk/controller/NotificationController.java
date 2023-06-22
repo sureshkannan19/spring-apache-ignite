7884c2a91247f4a6c530eeb3ffae2fd07ddec5d3
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2023-06-22 08:55:00 +0530

    added restassured API Test

--- src/main/java/com/sk/controller/NotificationController.java
+++ src/main/java/com/sk/controller/NotificationController.java
@@ -1,9 +1,6 @@
 package com.sk.controller;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
