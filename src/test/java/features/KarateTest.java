3d755be5259d1033eb4dc6d06797b09ffd9e822f
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2023-11-05 21:38:49 +0530

    build fix

--- src/test/java/features/KarateTest.java
+++ src/test/java/features/KarateTest.java
@@ -11,20 +11,20 @@ import com.sk.SpringwebApplication;
 
 public class KarateTest {
 
-	private static ConfigurableApplicationContext applicationContext;
+    private static ConfigurableApplicationContext applicationContext;
 
-//	@BeforeAll
-//	public static void setUp() {
-//		applicationContext = SpringwebApplication.start(new String[] { "-Dkarate.env=unit" });
-//	}
-//
-//	@AfterAll
-//	public static void stop() {
-//		SpringwebApplication.stop(applicationContext);
-//	}
-
-	@Test
-	public Karate runTest() {
-		return Karate.run("notification.feature").relativeTo(getClass());
+	@BeforeAll
+	public static void setUp() {
+		applicationContext = SpringwebApplication.start(new String[] { "-Dkarate.env=unit" });
 	}
+
+	@AfterAll
+	public static void stop() {
+		SpringwebApplication.stop(applicationContext);
+	}
+
+    @Test
+    public Karate runTest() {
+        return Karate.run("notification.feature").relativeTo(getClass());
+    }
 }
